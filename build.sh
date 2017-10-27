16bbde3b6026bca1c5142cf66bd3937c89387c21
李梦楠 <lmnhot@hotmail.com>
2017-10-27 12:47:46 +0800

    * pm2 reload

--- build.sh
+++ build.sh
@@ -2,6 +2,5 @@
 
 /usr/local/bin/cnpm install
 npm run build
-/usr/local/bin/pm2 stop upig
-/usr/local/bin/pm2 start ./server/index.js --name="upig" --max_memory_restart 1024M
+/usr/local/bin/pm2 reload upig
 
